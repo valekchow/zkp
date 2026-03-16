0a06139c248425cd45af94ce50c2d50deb50cebe
valekchow <148901008+valekchow@users.noreply.github.com>
2026-03-16 15:32:48 +0800

    Delete total-supply.js

--- total-supply.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Cache-Control", "public, max-age=86400");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).send("1000000000");
-}
